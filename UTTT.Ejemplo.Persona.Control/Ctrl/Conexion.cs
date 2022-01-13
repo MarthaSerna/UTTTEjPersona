a89e0b1b4c07e1d827aef6584bde2223c3314409
MarthaSerna <63262590+MarthaSerna@users.noreply.github.com>
2022-01-13 01:18:57 -0600

    EjemploPersonaUTTT, Tarea 1-Unidad 1

---
+++ UTTT.Ejemplo.Persona.Control/Ctrl/Conexion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace UTTT.Ejemplo.Persona.Control.Ctrl
+{
+    public class Conexion
+    {
+        #region Variables
+     
+        #endregion
+
+        #region Constructores
+        public Conexion()
+        {
+        }       
+        #endregion
+
+
+        public SqlConnection sqlConnection()
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection("Data Source=LAPTOP-UO7C7U71\\SQLEXPRESS;Initial Catalog=Persona;User ID=sa;Password=1234");
+                return conexion;
+            }
+            catch (Exception _e)
+            { 
+            
+            }
+            return null;
+        }
+    }
+}
